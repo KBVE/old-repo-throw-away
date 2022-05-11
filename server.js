09f3f5b5040bec8ada6e1f89a442e41f5687dd21
h0lybyte <admin@kbve.com>
2022-05-11 11:55:40 -0400

    Cleaning up Meme.sh
    5 years of old code, RIP.

--- server.js
+++
@@ -1,8 +0,0 @@
-const serve = require('koa-static');
-const Koa = require('koa');
-const app = new Koa();
-
-app.use(serve(__dirname + '/html5up-multiverse'));
-app.listen(3000, '0.0.0.0');
-
-console.log('listening on port 3000'); 
